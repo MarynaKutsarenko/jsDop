ed78c70d4b8cc6dd688701f594a58f7ab079ba09
MarynaKutsarenko <49794741+MarynaKutsarenko@users.noreply.github.com>
2019-04-25 18:10:02 +0200

    new

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+    let num = 3*3*7*2*1; 
+        console.log(num);
+
+    let power = (num ** 3);
+
+function mat() {
+    let cut = (power.toString().substring(0, 2));
+    alert(cut);
+}
+mat(); 
